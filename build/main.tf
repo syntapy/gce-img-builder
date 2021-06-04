cbe8980596e34210d459e3d63948ed1a7d449851
Sami Abdul-Wahid <syntapy@pm.me>
2021-06-03 19:15:35 -0700

    Change build trigger tag regex

--- build/main.tf
+++ build/main.tf
@@ -4,18 +4,10 @@ variable "zone" {
 
 resource "google_sourcerepo_repository" "machine_image" {
   name = "machine-image"
-
-  depends_on = [
-    google_project_service.sourcerepo
-  ]
 }
 
 resource "google_sourcerepo_repository" "packer" {
   name = "packer"
-
-  depends_on = [
-    google_project_service.sourcerepo
-  ]
 }
 
 resource "google_cloudbuild_trigger" "packer_build" {
@@ -36,7 +28,7 @@ resource "google_cloudbuild_trigger" "machine_image_build" {
   name = "machine-image"
   trigger_template {
     repo_name = google_sourcerepo_repository.machine_image.name
-    branch_name = "main"
+    tag_name = ".*"
   }
 
   filename = "cloudbuild.yaml"
