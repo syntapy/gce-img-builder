2f17556f156568114bb534487242cdda6babc0ca
Sami Abdul-Wahid <syntapy@pm.me>
2021-07-25 23:02:03 -0700

    Split into variables.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,6 @@
 // m5a.large
 // 2 cpus, 8gb ram, 20gbb hdd
 
-variable "region" { type = string }
-variable "zone_letter" { type = string }
-variable "project_name" { type = string }
-variable "project_number" { type = string }
-variable "project_id" { type = string }
-
 locals {
   cloud_build_account = join("@", [var.project_number, "cloudbuild.gserviceaccount.com"])
   cloud_build_member  = join(":", ["serviceAccount", local.cloud_build_account])
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  type = string
+}
+
+variable "zone_letter" {
+  type        = string
+  description = "Single letter to be appended to region to form zone"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project display name"
+}
+
+variable "project_number" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
